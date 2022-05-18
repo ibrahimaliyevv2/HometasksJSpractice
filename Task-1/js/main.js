0d6fa80e3e44f245906df25e8761320a380eb2a9
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-05-18 14:43:14 +0400

    Update main.js

--- Task-1/js/main.js
+++ Task-1/js/main.js
@@ -1,12 +1,7 @@
 let circle = document.querySelector(".circle");
-let windowBox = document.querySelector("window-box");
+let windowBox = document.querySelector(".window-box");
 
-windowBox.addEventListener("click",getClickPosition, false);
-
-function getClickPosition(e){
-    circle.offsetLeft = e.clientX - (circle.offsetWidth/2);
-    circle.offsetTop = e.clientY - (circle.offsetHeight/2);
-
-    let translate = "translate3d(" + offsetLeft + "px," + offsetTop + "px, 0)";
-    circle.style.transform = translate;
-}
+windowBox.addEventListener("click", e=>{
+    circle.style.left = e.clientX - (circle.offsetWidth/2) + "px";
+    circle.style.top = e.clientY - (circle.offsetHeight/2) + "px";
+})
